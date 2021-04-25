b4268e3ca6e18f908d3bceca74752420c645ee01
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2021-04-25 22:00:18 +0200

    Add implementation of RestrictedBankAccount

---
+++ src/it/unibo/oop/lab04/bank2/RestrictedBankAccount.java
@@ -0,0 +1,21 @@
+package it.unibo.oop.lab04.bank2;
+
+public class RestrictedBankAccount extends AbstractBankAccount {
+
+    private static final double TRANSACTION_FEE = 0.1;
+
+	public RestrictedBankAccount(double balance, int usrID) {
+		super(balance, usrID);
+	}
+
+	@Override
+	protected double computeFee() {
+		return AbstractBankAccount.MANAGEMENT_FEE + (getNTransactions() + RestrictedBankAccount.TRANSACTION_FEE);
+	}
+
+	@Override
+	protected boolean isWithdrawAllowed(double amount) {
+		return getBalance() > amount ? true: false;
+	}
+
+}
